3f84188649013193842c724783b88b2f3ad87fd1
tommaclean <tom.j.maclean@gmail.com>
2019-12-08 16:52:00 -0500

    fixed ProfileModal meetup rendering

--- src/ProfileModal.js
+++ src/ProfileModal.js
@@ -7,50 +7,59 @@ import ProfilePage from './ProfilePage'
 
 class ProfileModal extends React.Component {
   render(){
+    const pastMeetups = () => {
+      if (this.props.currentUserMeetups.length) {
+        return this.props.currentUserMeetups.reverse().map(userMeetup => {
+          return <PastMeetup key={userMeetup.meetup.id} id={userMeetup.meetup.id} meetup={userMeetup} name={userMeetup.meetup.name} vicinity={userMeetup.meetup.vicinity} handleFavoriteMeetup={this.props.handleFavoriteMeetup}/>
+        })
+      }
     return null
-  //   const pastMeetups = this.props.currentUserMeetups.reverse().map(userMeetup => {
-  //     return <PastMeetup key={userMeetup.meetup.id} id={userMeetup.meetup.id} meetup={userMeetup} name={userMeetup.meetup.name} vicinity={userMeetup.meetup.vicinity} handleFavoriteMeetup={this.props.handleFavoriteMeetup}/>
-  //   })
-  //   const userFavs = this.props.currentUserMeetupFavs.reverse().map(fav => {
-  //     return <MeetupFav key={fav.id} fav={fav} name={fav.name} vicinity={fav.vicinity} handleDeleteFav={this.props.handleDeleteFav} icon={fav.icon}/>
-  //   })
-  //
-  //   const panes = [
-  //     {
-  //       menuItem: 'Past Meetups',
-  //       render: () => <Tab.Pane attached={false}>
-  //         <Header>Past Meetups</Header>
-  //           <Item.Group>
-  //             { pastMeetups }
-  //           </Item.Group>
-  //       </Tab.Pane>,
-  //     },
-  //     {
-  //       menuItem: 'Favorite Meetups',
-  //       render: () => <Tab.Pane attached={false}><Header>Favorite Meetups</Header>
-  //       <Item.Group>
-  //       { userFavs }
-  //       </Item.Group>
-  //       </Tab.Pane>,
-  //     },
-  //   ]
-  //   return(
-  //     <Modal open={this.props.modalOpen}>
-  //         <Modal.Header>{this.props.currentUser.username}'s Profile</Modal.Header>
-  //             <Modal.Content image scrolling>
-  //               <Image size='medium' src='meet-in-the-middle-logo.jpg' wrapped />
-  //                 <Tab menu={{ pointing: true }} panes={panes}/>
-  //             </Modal.Content>
-  //             <Modal.Actions>
-  //               <Button primary onClick={this.props.handleProfileClick}>
-  //                 Close
-  //               </Button>
-  //             </Modal.Actions>
-  //           </Modal>
-  //   )
-  // }
+  }
+    const userFavs = () => {
+      if (this.props.currentUserMeetupFavs.length) {
+        return this.props.currentUserMeetupFavs.reverse().map(fav => {
+          return <MeetupFav key={fav.id} fav={fav} name={fav.name} vicinity={fav.vicinity} handleDeleteFav={this.props.handleDeleteFav} icon={fav.icon}/>
+        })
+    }
+    return null
+  }
+
+    const panes = [
+      {
+        menuItem: 'Past Meetups',
+        render: () => <Tab.Pane attached={false}>
+          <Header>Past Meetups</Header>
+            <Item.Group>
+              { pastMeetups() }
+            </Item.Group>
+        </Tab.Pane>,
+      },
+      {
+        menuItem: 'Favorite Meetups',
+        render: () => <Tab.Pane attached={false}><Header>Favorite Meetups</Header>
+        <Item.Group>
+        { userFavs() }
+        </Item.Group>
+        </Tab.Pane>,
+      },
+    ]
+    return(
+      <Modal open={this.props.modalOpen}>
+          <Modal.Header>{this.props.currentUser.username}'s Profile</Modal.Header>
+              <Modal.Content image scrolling>
+                <Image size='medium' src='meet-in-the-middle-logo.jpg' wrapped />
+                  <Tab menu={{ pointing: true }} panes={panes}/>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button primary onClick={this.props.handleProfileClick}>
+                  Close
+                </Button>
+              </Modal.Actions>
+            </Modal>
+    )
+  }
 }
-}
+
 
 
 
